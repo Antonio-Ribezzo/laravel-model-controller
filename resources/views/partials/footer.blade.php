1275e05067d6656809cba8f333c5faded7db018e
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-09 15:56:02 +0200

    Footer (Style)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,39 @@
-<footer>
-    <h2>Contenuto footer</h2>
+<footer class="py-5">
+    <div class="container">
+        <div class="text-white mb-4">
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        {{-- nav footer --}}
+        <div id="navFooter" class="d-flex justify-content-between align-items-centermb-3">
+            <div>
+                <a href=""><span class="d-block text-capitalize">audiodescrizione</span></a>
+                <a href=""><span class="d-block">Rapporti con gli investitori</span></a>
+                <a href=""><span class="d-block">Note legali</span></a>
+            </div>
+            <div>
+                <a href=""><span class="d-block">Centro assistenza</span></a>
+                <a href=""><span class="d-block">Opportunità di lavoro</span></a>
+                <a href=""><span class="d-block">Preferenze per i cookie</span></a>
+            </div>
+            <div>
+                <a href=""><span class="d-block">Carte regalo</span></a>
+                <a href=""><span class="d-block">Condizioni di utilizzo</span></a>
+                <a href=""><span class="d-block">Informazioni sull'azienda</span></a>
+            </div>
+            <div>
+                <a href=""><span class="d-block text-capitalize">media center</span></a>
+                <a href=""><span class="d-block text-capitalize">privacy</span></a>
+                <a href=""><span class="d-block text-capitalize">contattaci</span></a>
+            </div>
+        </div>
+        {{-- button footer --}}
+
+        <button type="button" class="btn btn-outline-secondary">Codice di servizio</button>
+
+        {{-- copyright  --}}
+        <span class="d-block mt-3"> &copy; 1997-2023 Boolflix, Inc.</span>
+    </div>
 </footer>
