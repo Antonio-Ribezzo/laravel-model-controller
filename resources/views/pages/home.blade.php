b70a8ce2a4238d5fda4d869bbe1c469168597395
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-09 16:40:59 +0200

    Main (Style) - Loop  from DB

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,10 +6,22 @@
 
 
 @section('contentHome') 
-    <main>
-        <h2 class="text-primary">Contenuto main</h2>
-        @foreach ($arrayMovies as $el )
-            <h3>{{ $el['title'] }}</h3>
-        @endforeach
+    <main class="px-5">
+        <!-- inserisco le card -->
+        <!-- FILM -->
+        <h2 class="text-white text-capitalize">film</h2>
+        <div id="container" class="row justify-content-center flex-wrap gap-3">
+            @foreach ( $arrayMovies as $el )
+                <div class="card col-2 pt-2">
+                    <img class="card-img-top" src="{{$el['poster']}}" alt="Title">
+                    <div class="card-body">
+                        <h4 class="card-title">{{$el['title']}}</h4>
+                        <p class="card-text">Vote: {{$el['vote']}}</p>
+                        <p class="card-text my-1">Titolo Originale: {{$el['original_title']}}</p>
+                        <p class="card-text">{{$el['date']}}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </main>
 @endsection
