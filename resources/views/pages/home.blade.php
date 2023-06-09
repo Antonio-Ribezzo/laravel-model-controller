5851dc5f898bdfffb37419149ea1aa80a5091ad1
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-09 15:03:13 +0200

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('titlePage') 
+    Boolflix
+@endsection
+
+
+@section('contentHome') 
+    <main>
+        <h2 class="text-primary">Contenuto main</h2>
+    </main>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <h2>Contenuto footer</h2>
+</footer>
