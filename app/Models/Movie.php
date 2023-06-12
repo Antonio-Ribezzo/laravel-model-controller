a1133e227649f679a10f7a2624264d88b47dc245
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-12 11:00:47 +0200

    Table name in Controller

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    // table name (serve nel caso in cui non dovesse riconoscere il nome della tabella)
+    // bisogna inserire il nome della tabella e non il nome del database
+    protected $table = 'movies';
 }
